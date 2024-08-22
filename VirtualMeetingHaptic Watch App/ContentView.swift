159d41e8e8ecc49428b2df9ca69366a2b9c1f29e
Alexloucy <alexloucy@icloud.com>
2024-08-22 17:32:54 +1200

    Initial Commit

---
+++ VirtualMeetingHaptic Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VirtualMeetingHaptic Watch App
+//
+//  Created by Alex Lou on 2024/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VirtualMeetingHaptic Watch App/VirtualMeetingHapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualMeetingHapticApp.swift
+//  VirtualMeetingHaptic Watch App
+//
+//  Created by Alex Lou on 2024/8/22.
+//
+
+import SwiftUI
+
+@main
+struct VirtualMeetingHaptic_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
