3b12944ae6ad910df2e7aae35855fea82ad9a3dd
Alexloucy <alexloucy@icloud.com>
2024-09-04 12:00:44 +1200

    add haptic feedback selection, heart rate selection currently broken, need new view

--- VirtualMeetingHaptic Watch App/ContentView.swift
+++ VirtualMeetingHaptic Watch App/ContentView.swift
@@ -5,11 +5,49 @@ struct ContentView: View {
     @State private var isVibrating = false
     @State private var heartRate: Int = 65
     @State private var timer: Timer?
+    enum HapticTypes: String, CaseIterable, Identifiable {
+           case notification, success, failure, retry, start, stop, click, directionUp, directionDown
+           var id: Self { self }
+
+           func toWKHapticType() -> WKHapticType {
+               switch self {
+               case .notification:
+                   return .notification
+               case .success:
+                   return .success
+               case .failure:
+                   return .failure
+               case .retry:
+                   return .retry
+               case .start:
+                   return .start
+               case .stop:
+                   return .stop
+               case .click:
+                   return .click
+               case .directionUp:
+                   return .directionUp
+               case .directionDown:
+                   return .directionDown
+               }
+           }
+       }
+//    let hapticTypes: [WKHapticType] = [.notification, .directionUp, .directionDown, .success, .failure, .retry, .start, .stop, .click]
+    @State private var selectedHaptic: HapticTypes  = .click
     
     var body: some View {
         VStack {
             Text("Heart Rate: \(heartRate) bpm")
                 .font(.system(.headline, design: .rounded))
+            
+           
+            Picker("Haptic Type", selection: $selectedHaptic) {
+                    ForEach(HapticTypes.allCases) { hapticType in
+                        Text(hapticType.rawValue.capitalized)
+                            .tag(hapticType)
+                }
+            }
+            .pickerStyle(.wheel)
             
             Button(action: {
                 isVibrating.toggle()
@@ -41,7 +79,7 @@ struct ContentView: View {
     
     private func startVibration() {
         timer = Timer.scheduledTimer(withTimeInterval: 1.0 / (Double(heartRate) * 1.3 / 60.0), repeats: true) { _ in
-            WKInterfaceDevice.current().play(.click)
+            WKInterfaceDevice.current().play(selectedHaptic.toWKHapticType())
         }
     }
     
